3a935f3716a0dbd92db7f2fcea8702d904aa796f
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-06-23 17:07:43 +0545

    Add login route with validation and JWT token generation; update user service and hashing utility

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { db } from "../db/index.js";
 import { usersTable } from "../models/user.model.js";
-import { signupPostRequestBodySchema } from "../validation/req.validation.js";
+import {
+  loginPostRequestBodySchema,
+  signupPostRequestBodySchema,
+} from "../validation/req.validation.js";
 import { hashPasswordWithSalt } from "../utils/hash.js";
 import { createUser, getUserByEmail } from "../services/user.service.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -39,6 +43,39 @@ router.post("/signup", async (req, res) => {
   });
 
   return res.status(201).json({ data: { userId: user.id } });
+});
+
+router.post("/login", async (req, res) => {
+  const validationResult = await loginPostRequestBodySchema.safeParseAsync(
+    req.body,
+  );
+
+  if (validationResult.error) {
+    return res.status(400).json({ error: validationResult.error });
+  }
+
+  const { email, password } = validationResult.data;
+
+  const user = await getUserByEmail(email);
+
+  if (!user) {
+    return res
+      .status(404)
+      .json({ error: `User with email ${email} does not exist!` });
+  }
+
+  const { password: hashedPassword } = hashPasswordWithSalt(
+    password,
+    user.salt,
+  );
+
+  if (user.password !== hashedPassword) {
+    return res.status(400).json({ error: "Invalid Password" });
+  }
+
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+
+  return res.json({ token });
 });
 
 export default router;
--- services/user.service.js
+++ services/user.service.js
@@ -9,6 +9,8 @@ export async function getUserByEmail(email) {
       firstname: usersTable.firstname,
       lastname: usersTable.lastname,
       email: usersTable.email,
+      salt: usersTable.salt,
+      password: usersTable.password,
     })
     .from(usersTable)
     .where(eq(usersTable.email, email));
