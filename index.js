4b72fab245b172b3dc58978c14fb6eb699ba7b33
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-06-23 16:11:56 +0545

    Implement user signup route with email uniqueness check and password hashing

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT ?? 8000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  return res.json({status:"Server is up and running..."})
+  return res.json({ status: "Server is up and running..." });
 });
 
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
---
+++ routes/user.routes.js
@@ -0,0 +1,41 @@
+import express from "express";
+import { db } from "../db/index.js";
+import { usersTable } from "../models/user.model.js";
+import { eq } from "drizzle-orm";
+import { createHmac, randomBytes } from "crypto";
+
+const router = express.Router();
+
+router.post("/signup", async (req, res) => {
+  const { firstname, lastname, email, password } = req.body;
+
+  const [existingUser] = await db
+    .select({
+      id: usersTable.id,
+    })
+    .from(usersTable)
+    .where(eq(usersTable.email, email));
+
+  if (existingUser)
+    res.status(400).json({ error: `User with email ${email} already exists!` });
+
+  const salt = randomBytes(256).toString("hex");
+  const hashedPassword = createHmac("sha256", salt)
+    .update(password)
+    .digest("hex");
+
+  const [user] = await db
+    .insert(usersTable)
+    .values({
+      email,
+      firstname,
+      lastname,
+      salt,
+      password: hashedPassword,
+    })
+    .returning({ id: usersTable.id });
+
+  return res.status(201).json({ data: { userId: user.id } });
+});
+
+export default router;
