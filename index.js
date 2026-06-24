f71eb4e7d199552ca47645c134e8693c655246db
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-06-24 16:07:45 +0545

    Add authentication middleware for token validation and authorization

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from "express";
-
+import { authenticationMiddleware } from "./middleware/auth.middleware.js";
 import userRouter from "./routes/user.routes.js";
 
 const app = express();
 const PORT = process.env.PORT ?? 8000;
 
 app.use(express.json());
+app.use(authenticationMiddleware);
 
 app.get("/", (req, res) => {
   return res.json({ status: "Server is up and running..." });
